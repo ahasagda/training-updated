e6af1a4004e3f5a0fd007fb237016d7184cb5382
aleynahasagdas <aleyna.hasagdas@boun.edu.tr>
2023-02-23 11:11:00 +0300

    Initial commit

---
+++ custom/training/traininginitialdata/gensrc/de/hybris/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Feb 17, 2023, 4:38:29 PM                    ---
+ * ----------------------------------------------------------------
+ *  
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
